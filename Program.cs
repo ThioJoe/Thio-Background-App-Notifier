5afe9b1727f9578099afbebf1ededfd4acf6e4b0
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2026-07-20 13:41:20 -0700

    Initial Commit - Project base

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace New_Startup_App_Notifier
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
